eff590bb3b363441db2893c70e0ec9717709db96
Jan Berdajs <mrbrdo@gmail.com>
2015-01-29 01:17:00 +0100

    datasource query attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :blogs, foreign_key: "owner_id"
   has_many :posts, foreign_key: "author_id"
   has_many :comments, foreign_key: "author_id"
+
+  datasource_module do
+    query(:full_name) { "first_name || ' ' || last_name" }
+  end
 end
