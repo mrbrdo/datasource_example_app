2448bedf09223cc2556883ec1fcc8b7d60044c9d
Jan Berdajs <mrbrdo@gmail.com>
2015-01-29 01:26:17 +0100

    loader example

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,21 @@
 class Blog < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   has_many :posts
+
+  datasource_module do
+    loaded :latest_post_title, from: :array
+
+    collection do
+      def load_latest_post_title
+        Post.where(blog_id: model_ids)
+        .having("id = MAX(id)")
+        .group(:blog_id)
+        .pluck(:blog_id, :title)
+      end
+    end
+  end
+
+  def latest_post_title
+    posts.order(:id).last.title
+  end
 end
--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,5 +1,5 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :latest_post_title
   has_one :owner
   has_many :posts
 end
